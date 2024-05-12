7c717c36af0287362895527388464254540b6395
Abhishek Kumar <abhishek117293@gmail.com>
2024-05-13 01:20:38 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      sans: ["GeneralSans-Variable", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        MainBackgroundColor: "#EEEEEF",
+      },
+    },
+  },
+  plugins: [],
+};
